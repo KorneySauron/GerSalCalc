ff395084cd39dc173494cda95233f4dbebdae8a0
KorneySauron <saukorn03@gmail.com>
2026-01-09 00:31:22 +0100

    Add Toolbar. Add theme of toolbar to themes.xml. Move Spinner to Toolbar.

--- app/src/main/java/com/korsau/gersalcalc/MainActivity.java
+++ app/src/main/java/com/korsau/gersalcalc/MainActivity.java
@@ -3,6 +3,7 @@ package com.korsau.gersalcalc;
 import android.os.Bundle;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -23,6 +24,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
 
         final List<String> langs = Arrays.asList("EN", "DE", "RU");
         final Spinner spinner = (Spinner) findViewById(R.id.langSelector); //lang selector is in activity_main xml

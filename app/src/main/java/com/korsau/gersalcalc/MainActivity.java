1bfebc272222a9147e1c5fddf06c1dd15cfebf97
KorneySauron <saukorn03@gmail.com>
2026-01-13 11:39:34 +0100

    Enable Edge-To-Edge. Optimizing lang_dropdown_item.xml

--- app/src/main/java/com/korsau/gersalcalc/MainActivity.java
+++ app/src/main/java/com/korsau/gersalcalc/MainActivity.java
@@ -6,8 +6,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowCompat;
 import androidx.core.view.WindowInsetsCompat;
 import android.view.View;
+import android.view.Window;
+import static android.view.Window.*;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.Spinner;
@@ -19,13 +22,26 @@ import com.korsau.gersalcalc.spinner.*;
 
 public class MainActivity extends AppCompatActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        ViewCompat.setOnApplyWindowInsetsListener(toolbar, (v, insets) -> {
+            Insets sysBars = insets.getInsets(WindowInsetsCompat.Type.statusBars());
+            v.setPadding(v.getPaddingLeft(), sysBars.top, v.getPaddingRight(), v.getPaddingBottom());
+            return insets;
+        });
+        /* line 33-37
+        we summon ViewCompat's method setOnApplyWindowInsetsListener, give it the insets of the users
+        android system bars, the method gives the View padding which equals to insets in size which
+        allows the app to adapt edge-to-edgeto for each device.
+        */
 
 
         final List<String> langs = Arrays.asList("EN", "DE", "RU");
